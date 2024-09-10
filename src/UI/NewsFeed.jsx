bd0202a40f5c1bc2f2ee6658693a3777bc2bdb33
Srdjan Cukic <cukic.srdjan@gmail.com>
2024-09-10 22:58:00 +0200

    Rearranged and better grouping of header elements

--- src/UI/NewsFeed.jsx
+++ src/UI/NewsFeed.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import ArticleSearch from "./ArticleSearch";
-import useApiFetch from "../service/useApiFetch";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ArticleSearch from './ArticleSearch';
+import useApiFetch from '../service/useApiFetch';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Backdrop } from '@mui/material';
 
 const NewsFeed = () => {
   const { params } = useParams();
-  const [sources] = params.split("-");
+  const [sources] = params.split('-');
   const [combinedData, setCombinedData] = useState([]);
   const [sortedData, setSortedData] = useState([]);
-  const [sortOption, setSortOption] = useState("newer");
+  const [sortOption, setSortOption] = useState('newer');
 
   const {
     data: nyTimes,
     isLoading: isLoading1,
     error: error1,
   } = useApiFetch(
-    "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=cg4vzM2CAqeJM2Ojw8zIZHZyEs40LVhS"
+    'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=cg4vzM2CAqeJM2Ojw8zIZHZyEs40LVhS',
   );
 
   const {
@@ -23,7 +25,7 @@ const NewsFeed = () => {
     isLoading: isLoading2,
     error: error2,
   } = useApiFetch(
-    "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f5f8bc54714e4aad8ee2fbca3b0573cd"
+    'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f5f8bc54714e4aad8ee2fbca3b0573cd',
   );
 
   const {
@@ -31,7 +33,7 @@ const NewsFeed = () => {
     isLoading: isLoading3,
     error: error3,
   } = useApiFetch(
-    "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=2af35c159a71bbcede539fe4c21ad3e5"
+    'https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=2af35c159a71bbcede539fe4c21ad3e5',
   );
 
   useEffect(() => {
@@ -45,50 +47,50 @@ const NewsFeed = () => {
     ) {
       let combinedArray = [];
 
-      const sourcesArray = sources.split(",");
+      const sourcesArray = sources.split(',');
 
       if (
         nyTimes &&
         Array.isArray(nyTimes.results) &&
-        sourcesArray.includes("The New York Times")
+        sourcesArray.includes('The New York Times')
       ) {
         combinedArray = combinedArray.concat(
-          nyTimes.results.map((article) => ({
+          nyTimes.results.map(article => ({
             ...article,
-            source: "The New York Times",
-          }))
+            source: 'The New York Times',
+          })),
         );
       }
 
       if (
         newsApi &&
         Array.isArray(newsApi.articles) &&
-        sourcesArray.includes("News Api")
+        sourcesArray.includes('News Api')
       ) {
         combinedArray = combinedArray.concat(
-          newsApi.articles.map((article) => ({
+          newsApi.articles.map(article => ({
             ...article,
-            source: "News Api",
-          }))
+            source: 'News Api',
+          })),
         );
       }
 
       if (
         gNewsApi &&
         Array.isArray(gNewsApi.articles) &&
-        sourcesArray.includes("Gnews")
+        sourcesArray.includes(' Gnews')
       ) {
         combinedArray = combinedArray.concat(
-          gNewsApi.articles.map((article) => ({
+          gNewsApi.articles.map(article => ({
             ...article,
-            source: "Gnews",
-          }))
+            source: ' Gnews',
+          })),
         );
       }
 
       // Final filter to remove objects with content marked as "[Removed]"
       const finalCombinedArray = combinedArray.filter(
-        (item) => item.content !== "[Removed]"
+        item => item.content !== '[Removed]',
       );
 
       setCombinedData(finalCombinedArray);
@@ -109,18 +111,18 @@ const NewsFeed = () => {
   useEffect(() => {
     let sortedArray = [...combinedData];
     switch (sortOption) {
-      case "newer":
+      case 'newer':
         sortedArray.sort(
           (a, b) =>
             new Date(b.created_date || b.publishedAt) -
-            new Date(a.created_date || a.publishedAt)
+            new Date(a.created_date || a.publishedAt),
         );
         break;
-      case "older":
+      case 'older':
         sortedArray.sort(
           (a, b) =>
             new Date(a.created_date || a.publishedAt) -
-            new Date(b.created_date || b.publishedAt)
+            new Date(b.created_date || b.publishedAt),
         );
         break;
       default:
@@ -129,14 +131,21 @@ const NewsFeed = () => {
     setSortedData(sortedArray);
   }, [sortOption, combinedData]);
 
-  const handleSortChange = (event) => {
+  const handleSortChange = event => {
     setSortOption(event.target.value);
   };
 
   // Render content based on the state of the API call
 
   if (isLoading1 && isLoading2 && isLoading3) {
-    return <div>Loading...</div>;
+    return (
+      <Backdrop
+        sx={theme => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        open={true}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
   }
 
   if (error1 && error2 && error3) {
